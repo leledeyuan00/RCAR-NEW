7fc627dd15ba6d4672ef980cb1bff5411e2330aa
Dayuan <373556662@qq.com>
2019-11-08 10:50:47 +0800

    fix base-url

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const http = axios.create({
-    baseURL: 'http://localhost:3000/admin/api'
-})
+  baseURL: process.env.VUE_APP_API_URL || "/admin/api"
+});
 
-export default http
+export default http;

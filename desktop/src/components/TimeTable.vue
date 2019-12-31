d69c60c406066050eea7a224c7a52669b1d26337
dayuan <373556662@qq.com>
2020-01-01 01:35:19 +0800

    Edit Timetable of Desktop --by xtl and of mobile --by cdy

--- desktop/src/components/TimeTable.vue
+++ desktop/src/components/TimeTable.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="table">
-    <!-- <template slot-scope="scope"> -->
-      <el-table :data="tableData" :row-class-name="tableRowClassName" :show-header="false" @row-click="handleClick">
-        <el-table-column prop="info" min-width="400"></el-table-column>
-        <el-table-column prop="date" min-width="100"></el-table-column>
-      </el-table>
-    <!-- </template> -->
+    <el-table
+      :data="tableData"
+      :cell-class-name="tableColClassName"
+      :row-class-name="tableRowClassName"
+      :show-header="false"
+      @row-click="handleClick"
+    >
+      <el-table-column prop="info" min-width="400"></el-table-column>
+      <el-table-column prop="old_date" min-width="100"></el-table-column>
+      <el-table-column prop="date" min-width="100"></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -17,16 +22,23 @@
 .el-table .success-row {
   background: #90dad9;
 }
-.table{
+.table {
   max-width: 800px;
-  margin:auto;
+  margin: auto;
+}
+.old {
+  color: rgb(0, 0, 0);
+  text-decoration: line-through;
+}
+.new {
+  color: red;
 }
 </style>
 
 <script>
 export default {
   methods: {
-    tableRowClassName({rowIndex }) {
+    tableRowClassName({ rowIndex }) {
       if (rowIndex === 1) {
         return "warning-row";
       } else if (rowIndex === 3) {
@@ -34,9 +46,17 @@ export default {
       }
       return "";
     },
+    // by xtl 2019.12.31
+    tableColClassName({ columnIndex }) {
+      if (columnIndex === 1) {
+        return "old";
+      } else if (columnIndex === 2) {
+        return "new";
+      }
+      return "";
+    },
     handleClick(row) {
-      if(row.url)
-        this.$router.push(row.url);
+      if (row.url) this.$router.push(row.url);
     }
   },
   data() {
@@ -45,24 +65,29 @@ export default {
         {
           date: "Jan.31,2020",
           info: "Submission of papers",
+          old_date: "Dec.31,2019",
           url: "./InitialSubmission"
         },
         {
           date: "Jan.31,2020",
           info: "Submission of organized session/workshop",
+          old_date: "Dec.31,2019"
         },
         {
           date: "Mar.20,2020",
           info: "Notificationof acceptance",
+          old_date: "Feb.20,2020"
         },
         {
           date: "Apr.20,2020",
           info: "Final paper submission",
+          old_date: "Mar.20,2020",
           url: "./FinalSubmission"
         },
         {
           date: "May.1,2020",
           info: "Early bird registration",
+          old_date: "",
           url: "./Registration"
         }
       ]
--- web/src/components/Table.vue
+++ web/src/components/Table.vue
@@ -14,37 +14,17 @@
         </b-tr>
       </b-thead>
       <b-tbody class="fs-xl">
-        <b-tr class="bg-light">
+        <b-tr class="bg-light" v-for="table in tableDate" :key="table">
           <b-th colspan="4" class="text-left fw-s" rowspan="1">
-            <router-link :to="'./InitialSubmission'">
-              Submission of papers
-            </router-link>
-            </b-th>
-          <b-th class="text-center fw-s">Jan.31,2020</b-th>
-        </b-tr>
-        <b-tr class="bg-light">
-          <b-th
-            colspan="4"
-            class="text-left fw-s"
-            rowspan="1"
-          >Submission of organized session/workshop</b-th>
-          <b-th class="text-center fw-s">Jan.31,2020</b-th>
-        </b-tr>
-        <b-tr class="bg-light">
-          <b-th colspan="4" class="text-left fw-s" rowspan="1">Notificationof acceptance</b-th>
-          <b-th class="text-center fw-s">Mar.20,2020</b-th>
-        </b-tr>
-        <b-tr class="bg-light">
-          <b-th colspan="4" class="text-left fw-s" rowspan="1">
-            <router-link :to="'./FinalSubmission'">
-              Final paper submission
-            </router-link>
-            </b-th>
-          <b-th class="text-center fw-s">Apr.20,2020</b-th>
-        </b-tr>
-        <b-tr class="bg-light">
-          <b-th colspan="4" class="text-left fw-s" rowspan="1">Early bird registration</b-th>
-          <b-th class="text-center fw-s">May.1,2020</b-th>
+            <div v-if="table.url">
+              <router-link :to="table.url">
+                {{ table.info }}
+              </router-link>
+            </div>
+            <div v-else>{{table.info}}</div>
+          </b-th>
+          <b-th class="text-center fw-s old">{{ table.old_date }}</b-th>
+          <b-th class="text-center fw-s new">{{ table.date }}</b-th>
         </b-tr>
       </b-tbody>
     </b-table-simple>
@@ -52,5 +32,51 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tableDate: [
+        {
+          date: "Jan.31,2020",
+          info: "Submission of papers",
+          old_date: "Dec.31,2019",
+          url: "./InitialSubmission"
+        },
+        {
+          date: "Jan.31,2020",
+          info: "Submission of organized session/workshop",
+          old_date: "Dec.31,2019"
+        },
+        {
+          date: "Mar.20,2020",
+          info: "Notificationof acceptance",
+          old_date: "Feb.20,2020"
+        },
+        {
+          date: "Apr.20,2020",
+          info: "Final paper submission",
+          old_date: "Mar.20,2020",
+          url: "./FinalSubmission"
+        },
+        {
+          date: "May.1,2020",
+          info: "Early bird registration",
+          old_date: "",
+          url: "./Registration"
+        }
+      ]
+    };
+  }
+};
 </script>
+
+<style lang="scss">
+.old {
+  color: grey;
+  text-decoration: line-through;
+  font-size: 14px;
+}
+.new {
+  color: red;
+}
+</style>

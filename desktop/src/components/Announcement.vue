fd2960247a8334e4e88c6c1854317495aecf4eab
TONGLIANG0711 <767560019@qq.com>
2020-07-02 20:58:45 +0800

    add Announcement.vue change announcement in home.vue

---
+++ desktop/src/components/Announcement.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-alert
+  title="Important Anouncement"
+  type="warning"
+  description="Since the current situation of COVID-19, the program committee has determined to postpone the conference. The registration site will be opened after the new schedule is determined. Please continue to follow the update in this website.
+  Program committee of RCAR 2020
+  July.1st"
+  center=true
+  show-icon
+  closable=false>
+  </el-alert>
+</template>
+
+
+
+<!--<template>
+  <div class="fs-xxl">
+      <p>Since the current situation of COVID-19, the program committee has determined to postpone the conference. The registration site will be opened after the new schedule is determined. Please continue to follow the update in this website. </p>
+    <div class="text-right">
+       <p>Program committee of RCAR 2020</p>
+      <p>July.1st</p> 
+    </div>
+  </div>
+</template>-->
+
+
+

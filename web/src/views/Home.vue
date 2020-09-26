fab10bad615194b0b4b65363f1725c2d6e49d0d6
TONGLIANG0711 <767560019@qq.com>
2020-09-26 13:45:41 +0800

    update Keynote Speakers and announcement for mobile client

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -7,13 +7,6 @@
       <i class="el-icon-warning pl-2 imp"></i>
     </div>
     <div class="fs-xl bg-light px-2 ">
-      <p style="word-break:break-all">If the Paper ID and Title are not displayed during registration, you can use the passcode received in the email to log in to 
-      <a href="https://www.softconf.com/j/rcar2020">www.softconf.com/j/rcar2020</a>
-       and complete your personal information in Revise Participant Registration.</p>
-    <div class="text-right">
-       <pp>Program committee of RCAR 2020</pp>
-       <p>Sep. 20, 2020</p> 
-    </div>
       <p>The registration has started on 
           <font color="#FF0000">
           Sep. 18

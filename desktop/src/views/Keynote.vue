409cf1fe7025dac6984beb71e16fae6b7a601b5c
TONGLIANG0711 <767560019@qq.com>
2020-09-26 09:56:49 +0800

    update Keynote Speakers

--- desktop/src/views/Keynote.vue
+++ desktop/src/views/Keynote.vue
@@ -5,7 +5,7 @@
       </h1>
       <br>
       <h3 style="margin-top:10px; margin-bottom:10px;">
-        <strong> Keynote Talk Ⅰ: 11:30-12:00, Monday, Sep. 28</strong>
+        <strong> Keynote Talk Ⅰ: 13:00-13:30, Monday, Sep. 28</strong>
       </h3>
       <br>
       <img src="./../../../src/images/XiangpengLi.png" class="card-im-left" width="175" height="222">
@@ -29,7 +29,31 @@
 
       <br>
       <h3 style="margin-top:10px; margin-bottom:10px;">
-        <strong> Keynote Talk Ⅱ: 11:30-12:00, Tuesday, Sep. 29</strong>
+        <strong> Keynote Talk Ⅱ: 13:30-14:00, Monday, Sep. 28</strong>
+      </h3>
+      <br>
+      <img src="./../../../src/images/FanZhang.png" class="card-im-left" width="175" height="222">
+      <br>
+      <h4>
+        <strong> Associate prof. Fan Zhang, Northwestern Polytechnical University, China</strong>
+      </h4>
+      <h4>
+        <strong>Title: From Rigid to Flexible: A New Solution for Space Debris Removal</strong>
+      </h4>
+      <h5>
+      <p style="text-align:justify">
+        <strong>Abstract:</strong>
+        Space debris has always been a growing problem worldwide because it takes up vast orbital resources and greatly threatens space safety. The development of the space tether application since the 1970s has revealed some promising new solutions to space debris capture and removal that are different from traditional rigid capturing via space manipulator. A brand new solution, namely a tethered space net robot is introduced in this talk, including the motivation, system design, challenges, and solutions.
+      </p>
+      <p style="text-align:justify">
+        <strong>Biography:</strong>
+        Fan Zhang has been an associate research professor in the school of Astronautics at Northwestern Polytechnical University since Feb. 2018. She received her PhD degree of Guidance, Navigation and Control in 2017 from Northwestern Polytechnical University, and during the PhD study, she was working in Mcgill University as a research trainee from 2012 to 2014. Her interest is about dynamics and control of space tethered systems including tethered space robot, space multi-tethers, and maneuverable tethered space net robot. 
+        </p>
+      </h5>
+
+      <br>
+      <h3 style="margin-top:10px; margin-bottom:10px;">
+        <strong> Keynote Talk Ⅲ: 13:00-13:30, Tuesday, Sep. 29</strong>
       </h3>
       <br>
       <img src="./../../../src/images/SONGZhan.png" class="card-im-left" width="175" height="222">
@@ -50,6 +74,32 @@
         Prof. SONG Zhan received his Ph. D. degree in Automation and Computer-Aided Engineering from the Chinese University of Hong Kong in 2008, and joined SIAT the following year. He is currently the assistant director of Institute of Advanced Integration Technology and the executive director of Research Laboratory for Intelligent Design and Machine Vision, he is also the technical director of Maker Institute CAS. His major field of research is 3D machine vision and pattern recognition. He has received several grants from the NSFC, Key-Area Research and Development Program of Guangdong Province, and the CAD Knowledge and Creativities Project etc. He has more than 40 related research articles which have been published in area top journals, such as the IEEE Trans. on PAMI, IEEE Trans. on IE, IEEE Trans. on IP, and CAD, etc. More than 40 patents of invention have been issued or applied in the past years, moreover, 10 of them have been transferred and transformed. 
         </p>
       </h5>
+
+      <br>
+      <h3 style="margin-top:10px; margin-bottom:10px;">
+        <strong> Keynote Talk Ⅳ: 13:30-14:00, Tuesday, Sep. 29</strong>
+      </h3>
+      <br>
+      <img src="./../../../src/images/LinWang.png" class="card-im-left" width="175" height="222">
+      <br>
+      <h4>
+        <strong> Prof. Lin Wang, Shanghai Jiao Tong University, China</strong>
+      </h4>
+      <h4>
+        <strong>Title:Controllability and Cooperation of Networked Multi-agent Systems</strong>
+      </h4>
+      <h5>
+      <p style="text-align:justify">
+        <strong>Abstract:</strong>
+        System controllability as a fundamental concept has been extensively studied over the last half a century. Although various criteria have been well developed, the controllability issue becomes more complicated and challenging when applied to large-scale networked multi-agent systems due to their interaction complexity. In this talk, some effective criteria for determining the large-scale networked system controllability are introduced, following by the discussions of robustness of controllability against various malicious attacks. Based on the controllability, distributed controllers are further designed for various multi-agent systems, including bounded control for connectivity maintenance of double-integrator agents, sampled-data control of nonholonomic multi-robot systems, cooperative tracking control subject to motion constraints, and pose synchronization of multiple rigid bodies.   
+      </p>
+      <p style="text-align:justify">
+        <strong>Biography:</strong>
+        Lin Wang is a Professor in the Department of Automation, Shanghai Jiao Tong University. She received her Ph.D. degree in control theory from Academy of Mathematics and Systems Science, Chinese Academy of Sciences in 2009. From Apr. 2010 to Sep. 2010 and Sep. 2011 to Feb. 2012, she was a Guest Researcher at Royal Institute of Technology (KTH), Sweden. From Jan. 2016 to Mar. 2016, she was a Research Fellow at the City University of Hong Kong. From Jan. 2018 to Aug. 2018, she was a Visiting Associate Professor at Harvard University, and from Aug. 2018 to Jan. 2019, she was a visiting Associate Professor at the Media Lab of Manchester Institute of Technology (MIT). She was a recipient of the Best Theory Paper Award at the 13th World Congress on Intelligent Control and Automation in 2018, and a recipient of the Excellent Scientific Paper in the Field of Electronic Information in 2020. She is a Senior Member of IEEE, and serves as the General Secretary of the Chinese Chapter of Network Science Society, General Secretary of the CSIAM Activity Group on Complex Networks and Complex Systems. Her current research interests are adaptive complex networks, and coordination of multi-agent systems.
+        </p>
+      </h5>
+
+
   </div>
 </template>
 

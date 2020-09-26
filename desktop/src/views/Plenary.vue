a4a68623805dbff8e3d3117a4011db7345d01c62
Dayuan <373556662@qq.com>
2020-09-27 00:45:24 +0800

    Update Plenary.vue

--- desktop/src/views/Plenary.vue
+++ desktop/src/views/Plenary.vue
@@ -168,7 +168,7 @@
         Engineering</strong
       >
     </h4>
-    <h5><strong>Web Meeting Room ID: <a href="https://meeting.tencent.com/s/2hEONK8mKaG">967350543</a></strong></h5>
+    <h5><strong>Web Meeting Room ID: <a href="https://meeting.tencent.com/s/2hEONCk8mKaG">967350543</a></strong></h5>
     <h5>
       <p style="text-align: justify">
         <strong>Abstract:</strong>
@@ -261,7 +261,7 @@
         Cable-suspended Transportation Systems</strong
       >
     </h4>
-    <h5><strong>Web Meeting Room ID: <a href="https://meeting.tencent.com/s/2hEONK8mKaG">967350543</a></strong></h5>
+    <h5><strong>Web Meeting Room ID: <a href="https://meeting.tencent.com/s/2hEONCk8mKaG">967350543</a></strong></h5>
     <h5>
       <p style="text-align: justify">
         <strong>Abstract:</strong>
